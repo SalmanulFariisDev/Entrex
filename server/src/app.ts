8681c07d4a23c7729df9d4644ee39548e6d5943b
SalmanulFariisDev <salmanulfaris.dev@gmail.com>
2025-08-07 00:17:11 +0530

    updated deploy issue

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,18 +8,20 @@ const app = express();
 
 const allowedOrigins = (process.env.FRONTEND_URLS || "").split(",");
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      console.log("❌ Blocked by CORS:", origin);
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true); // ✅ Allow request
+      } else {
+        console.log("❌ Blocked by CORS:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
